89dc1fdfd01fc2f0491d6becede716e58986a87a
dpak-maurya <dpak.m932@gmail.com>
2020-02-24 18:31:35 +0530

    added makefile for execution

---
+++ cmake-build-debug/CMakeFiles/Assign1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "Assign1"
+  "Assign1.pdb"
+  "CMakeFiles/Assign1.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Assign1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
